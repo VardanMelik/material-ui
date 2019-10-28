f8ade902ac414a0dad077120a1d542964c4bc266
EYE MOTION <eyemotiongamestudio@gmail.com>
2019-10-28 22:32:34 +0400

    TreeAndTable

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
-
+import StyledTreeItem from './components/layout/Tree';
+import EnhancedTableHead from './components/layout/Table';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <StyledTreeItem />
+      < EnhancedTableHead />
         Material UI
       <Footer />
     </div>
